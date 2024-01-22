4b7053245b377ad161d9683c74f1f46e5d9a8eca
saulprl <phasecross@hotmail.com>
2024-01-22 13:42:02 -0700

    Update treesitter and flutter specs

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -1,3 +1,12 @@
 return {
-  "thosakwe/vim-flutter",
+  "akinsho/flutter-tools.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim",
+  },
+  config = true,
+  init = function ()
+    require("flutter-tools").setup {}
+  end
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,16 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function ()
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-      ensure_installed = { "lua", "javascript", "typescript", "vim", "vimdoc", "html", "query" },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
+  build = function ()
+    require("nvim-treesitter.install").update({ with_sync = true })()
+  end,
+  init = function ()
+    require("nvim-treesitter.configs").setup {
+      ensure_installed = { "javascript", "typescript", "jsdoc", "dart", "lua", "html", "json", "yaml" },
+      sync_install = true,
+      auto_install = true,
+      highlight = {
+        enable = true,
+      }
+    }
   end
 }
