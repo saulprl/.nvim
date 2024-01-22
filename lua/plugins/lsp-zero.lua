0585b6566b7f20c6c0b14e49831ccdcbf09b325d
saulprl <phasecross@hotmail.com>
2024-01-21 18:11:58 -0700

    Replace packer.nvim with lazy.nvim

---
+++ lua/plugins/lsp-zero.lua
@@ -0,0 +1,62 @@
+return {
+  "VonHeikemen/lsp-zero.nvim",
+  branch = "v3.x",
+  dependencies = {
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+
+    -- LSP Support
+    { "neovim/nvim-lspconfig" },
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'hrsh7th/cmp-nvim-lua'},
+    {'saadparwaiz1/cmp_luasnip'},
+    {'rafamadriz/friendly-snippets'},
+    {'L3MON4D3/LuaSnip'},
+  },
+  init = function()
+    local lsp_zero = require('lsp-zero')
+
+    lsp_zero.on_attach(function(client, bufnr)
+      lsp_zero.default_keymaps({
+        buffer = bufnr,
+        preserve_mappings = false,
+      })
+    end)
+
+    require('mason').setup({})
+    require('mason-lspconfig').setup({
+      ensure_installed = {'tsserver', 'rust_analyzer'},
+      handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+          local lua_opts = lsp_zero.nvim_lua_ls()
+          require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+      }
+    })
+
+    local cmp = require('cmp')
+    local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+    cmp.setup({
+      sources = {
+        {name = 'path'},
+        {name = 'nvim_lsp'},
+        {name = 'nvim_lua'},
+        {name = 'luasnip', keyword_length = 2},
+        {name = 'buffer', keyword_length = 3},
+      },
+      formatting = lsp_zero.cmp_format(),
+      mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+      }),
+    })
+  end,
+}
